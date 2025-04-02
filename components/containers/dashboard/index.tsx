f74ecb68dd2df482d041c0ccee2cc2f8f4b6ecf8
waiooaung <waiooaung.sea@gmail.com>
2025-04-02 15:17:57 +0400

    feat: refactor DashboardTransactionOverview to display multiple transaction statistics with improved layout

--- components/containers/dashboard/index.tsx
+++ components/containers/dashboard/index.tsx
@@ -18,7 +18,7 @@ export default function DashboardContainer() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-2">
+      <div className="grid grid-cols-1 gap-4">
         <DashboardTransactionOverview />
       </div>
 
--- components/overviews/dashboard/transaction-overview.tsx
+++ components/overviews/dashboard/transaction-overview.tsx
@@ -2,18 +2,12 @@
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import {
-  ArrowLeftRight,
-  JapaneseYen,
-  Banknote,
-  DollarSign,
-  TrendingUp,
-} from "lucide-react";
 import Link from "next/link";
 
 import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 import { useAuth } from "@/context/authContext";
+import { Currency } from "@/types/currency";
 
 interface Data {
   totalTransactionsCount: number;
@@ -23,12 +17,16 @@ interface Data {
   totalCommission: number;
   totalEarnings: number;
   totalProfit: number;
+  totalRemainingAmountFromAgent: number;
+  totalRemainingAmountToPayToSupplier: number;
+  baseCurrency: Currency;
+  quoteCurrency: Currency;
 }
 interface ApiResponse {
   statusCode: number;
   success: boolean;
   message: string;
-  data: Data;
+  data: Data[];
 }
 
 const DashboardTransactionOverview = () => {
@@ -38,107 +36,119 @@ const DashboardTransactionOverview = () => {
     `/dashboard/transaction-statistics?tenantId=${tenantId}`,
     fetcher,
   );
-  const {
-    totalTransactionsCount = 0,
-    totalBaseAmount = 0,
-    totalQuoteAmountBuy = 0,
-    totalQuoteAmountSell = 0,
-    totalEarnings = 0,
-  } = data?.data || {};
+  const transactionStats = data?.data || [];
 
   useEffect(() => {
     if (error) {
       toast.error("Failed to load transaction statistics");
     }
   }, [error]);
-  return (
-    <>
-      <Card>
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle className="text-sm font-medium">Transactions</CardTitle>
-            <ArrowLeftRight className="h-4 w-4 text-muted-foreground" />
-          </div>
-          <p className="text-xs text-muted-foreground">Total Transactions</p>
-        </CardHeader>
-        <CardContent>
-          <Link href="/dashboard/transactions">
-            <div className="text-xl font-bold text-green-500 truncate">
-              {new Intl.NumberFormat("en-US").format(totalTransactionsCount)}
+  return transactionStats.map((stats, index) => {
+    return (
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4"
+        key={index}
+      >
+        <Card>
+          <CardHeader className="space-y-1">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-xs font-medium">
+                {stats.baseCurrency.name} - {stats.quoteCurrency.name}
+              </CardTitle>
             </div>
-          </Link>
-        </CardContent>
-      </Card>
+            <p className="text-xs text-muted-foreground">Total Transactions</p>
+          </CardHeader>
+          <CardContent>
+            <Link href="/dashboard/transactions">
+              <div className="font-bold text-green-500 truncate">
+                {new Intl.NumberFormat("en-US").format(
+                  stats.totalTransactionsCount,
+                )}
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle className="text-sm font-medium">Total</CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
-          </div>
-          <p className="text-xs text-muted-foreground">Received from Agents</p>
-        </CardHeader>
-        <CardContent>
-          <Link href="/dashboard/transactions">
-            <div className="text-xl font-bold text-green-500 truncate">
-              {new Intl.NumberFormat("en-US").format(totalQuoteAmountSell)}
+        <Card>
+          <CardHeader className="space-y-1">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-sm font-medium">
+                Total Profit ({stats.quoteCurrency.symbol})
+              </CardTitle>
             </div>
-          </Link>
-        </CardContent>
-      </Card>
+            <p className="text-xs text-muted-foreground">Profit + Commission</p>
+          </CardHeader>
+          <CardContent>
+            <Link href="/transactions">
+              <div className="text-xl font-bold text-green-500 truncate">
+                {new Intl.NumberFormat("en-US").format(stats.totalEarnings)}
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle className="text-sm font-medium">Total</CardTitle>
-            <Banknote className="h-4 w-4 text-muted-foreground" />
-          </div>
-          <p className="text-xs text-muted-foreground">Paid to Suppliers</p>
-        </CardHeader>
-        <CardContent>
-          <Link href="/dashboard/transactions">
-            <div className="text-xl font-bold text-green-500 truncate">
-              {new Intl.NumberFormat("en-US").format(totalQuoteAmountBuy)}
+        <Card>
+          <CardHeader className="space-y-1">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-sm font-medium">
+                Total ({stats.baseCurrency.symbol})
+              </CardTitle>
             </div>
-          </Link>
-        </CardContent>
-      </Card>
+            <p className="text-xs text-muted-foreground">Total Exchanged</p>
+          </CardHeader>
+          <CardContent>
+            <Link href="/dashboard/transactions">
+              <div className="text-xl font-bold text-green-500 truncate">
+                {new Intl.NumberFormat("en-US").format(stats.totalBaseAmount)}
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle className="text-sm font-medium">Total Profit</CardTitle>
-            <TrendingUp className="h-4 w-4 text-muted-foreground" />
-          </div>
-          <p className="text-xs text-muted-foreground">Profit + Commission</p>
-        </CardHeader>
-        <CardContent>
-          <Link href="/transactions">
-            <div className="text-xl font-bold text-green-500 truncate">
-              {new Intl.NumberFormat("en-US").format(totalEarnings)}
+        <Card>
+          <CardHeader className="space-y-1">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-sm font-medium">
+                Total ({stats.quoteCurrency.symbol})
+              </CardTitle>
             </div>
-          </Link>
-        </CardContent>
-      </Card>
+            <p className="text-xs text-muted-foreground">
+              Received from Agents
+            </p>
+          </CardHeader>
+          <CardContent>
+            <Link href="/dashboard/transactions">
+              <div className="text-xl font-bold text-green-500 truncate">
+                {new Intl.NumberFormat("en-US").format(
+                  stats.totalQuoteAmountSell,
+                )}
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle className="text-sm font-medium">Total</CardTitle>
-            <JapaneseYen className="h-4 w-4 text-muted-foreground" />
-          </div>
-          <p className="text-xs text-muted-foreground">Total Exchanged</p>
-        </CardHeader>
-        <CardContent>
-          <Link href="/dashboard/transactions">
-            <div className="text-xl font-bold text-green-500 truncate">
-              ¥{new Intl.NumberFormat("en-US").format(totalBaseAmount)}
+        <Card>
+          <CardHeader className="space-y-1">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-sm font-medium">
+                Total ({stats.quoteCurrency.symbol})
+              </CardTitle>
             </div>
-          </Link>
-        </CardContent>
-      </Card>
-    </>
-  );
+            <p className="text-xs text-muted-foreground">Paid to Suppliers</p>
+          </CardHeader>
+          <CardContent>
+            <Link href="/dashboard/transactions">
+              <div className="text-xl font-bold text-green-500 truncate">
+                {new Intl.NumberFormat("en-US").format(
+                  stats.totalQuoteAmountBuy,
+                )}
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  });
 };
 
 export default DashboardTransactionOverview;
