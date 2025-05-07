7247d834e69abab5c72f199b16a45e97d14680a2
waiooaung <waiooaung.sea@gmail.com>
2025-05-07 17:00:54 +0400

    refactor: clean up axios instance code; improve readability and error handling

--- lib/axios-instance.ts
+++ lib/axios-instance.ts
@@ -4,12 +4,14 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 import Cookies from "js-cookie";
+import { toast } from "sonner";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const axiosInstance = axios.create({
   baseURL: apiUrl,
   headers: {
-    Accept: `application/json`,
+    Accept: "application/json",
     "Access-Control": "Allow-Origin",
     "Content-Type": "application/json",
   },
@@ -18,25 +20,29 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
     const token = Cookies.get("token");
-
     if (config.headers) {
       config.headers.Authorization = token ? `Bearer ${token}` : "";
     }
     return config;
   },
-  (error: AxiosError) => {
-    return Promise.reject(error);
-  },
+  (error: AxiosError) => Promise.reject(error),
 );
 
 axiosInstance.interceptors.response.use(
-  async (response: AxiosResponse) => {
+  (response: AxiosResponse) => {
     if (!response.data) {
       return Promise.reject(response);
     }
     return response;
   },
-  async (error: AxiosError) => {
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      toast.error("Session expired. Logging out...");
+      Cookies.remove("token");
+      Cookies.remove("user");
+      return Promise.reject();
+    }
+
     return Promise.reject(error);
   },
 );
